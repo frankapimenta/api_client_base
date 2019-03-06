7f1bc898a05223ae1966f78dde85bc231aedbd4b
111443 <frank.pimenta@swisslife-select.ch>
2019-03-06 11:17:51 +0100

    % Move spec for version to its own file.

--- spec/api_client_base_spec.rb
+++ spec/api_client_base_spec.rb
@@ -1,6 +1,3 @@
 RSpec.describe ApiClientBase do
-  it "has a version number" do
-    expect(ApiClientBase::VERSION).to eq "0.1.0"
-  end
 
 end
---
+++ spec/version_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe ApiClientBase do
+  it "has a version number" do
+    expect(ApiClientBase::VERSION).to eq "0.1.0"
+  end
+end
+
