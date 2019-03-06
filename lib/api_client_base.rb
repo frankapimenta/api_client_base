7bade059e6f768694abcfd4e17924fc14ca0486f
111443 <frank.pimenta@swisslife-select.ch>
2019-03-06 11:21:36 +0100

    + Add spec to test presence of Error class.

--- lib/api_client_base.rb
+++ lib/api_client_base.rb
@@ -2,5 +2,4 @@ require "api_client_base/version"
 
 module ApiClientBase
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/api_client_base_spec.rb
+++ spec/api_client_base_spec.rb
@@ -1,3 +1,5 @@
 RSpec.describe ApiClientBase do
-
+  specify "has class Error defined" do
+    expect(ApiClientBase::Error).to be_a Class
+  end
 end
